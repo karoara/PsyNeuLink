26b2c68c38588cdf0fda52685aabf50f316a87aa
KristenManning <kristenmanning16@gmail.com>
2018-04-18 17:09:01 -0400

    undoing the assignment of 1s as inputs during simulations - this was not correct

--- psyneulink/library/subsystems/evc/evccontrolmechanism.py
+++ psyneulink/library/subsystems/evc/evccontrolmechanism.py
@@ -959,16 +959,16 @@ class EVCControlMechanism(ControlMechanism):
         for origin_mech in self.system.origin_mechanisms:
             # Get origin Mechanism for each process
             # Assign value of predictionMechanism to the entry of predicted_input for the corresponding ORIGIN Mechanism
-            if hasattr(self.system, "prediction_learning_origin_mechanisms"):
-                if origin_mech in self.system.prediction_learning_origin_mechanisms:
-                    self.predicted_input[origin_mech] = \
-                        np.ones_like(self.origin_prediction_mechanisms[origin_mech].output_state.value)
-                else:
-                    self.predicted_input[origin_mech] = \
-                        self.origin_prediction_mechanisms[origin_mech].output_state.value
-
-            else:
-                self.predicted_input[origin_mech] = self.origin_prediction_mechanisms[origin_mech].output_state.value
+            # if hasattr(self.system, "prediction_learning_origin_mechanisms"):
+            #     if origin_mech in self.system.prediction_learning_origin_mechanisms:
+            #         self.predicted_input[origin_mech] = \
+            #             np.ones_like(self.origin_prediction_mechanisms[origin_mech].output_state.value)
+            #     else:
+            #         self.predicted_input[origin_mech] = \
+            #             self.origin_prediction_mechanisms[origin_mech].output_state.value
+            #
+            # else:
+            self.predicted_input[origin_mech] = self.origin_prediction_mechanisms[origin_mech].output_state.value
 
     def run_simulation(self,
                        inputs,
